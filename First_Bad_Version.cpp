ffde19753ae9105c42110fdd62aeb63ceed2212d
Yudhajit Chowdhury <59924789+YudhajitChowdhury@users.noreply.github.com>
2020-05-02 14:46:07 +0530

    Create First_Bad_Version.cpp

---
+++ First_Bad_Version.cpp
@@ -0,0 +1,21 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left=0;
+        int right=n;
+        while(left<right){
+        int midpoint(left+(right-left)/2);
+        if(isBadVersion(midpoint)){
+            right=midpoint;
+        }else{
+            left=midpoint+1;
+        }
+        }
+        if(left==right && isBadVersion(left))
+            return left;
+        return -1;
+    }       
+};
